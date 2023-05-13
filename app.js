524763eb43d902aa1febd9780e7014964f4846a9
Darshan <darshanbpatel1904@gmail.com>
2023-05-13 23:57:33 +0530

    modified vercel.json

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ import { urouter } from './routes/userroutes.js';
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
+app.set("views", path.join(__dirname + "views"));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 
 app.use(brouter);
 app.use(urouter);
