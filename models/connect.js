6b484a99c8549615a07c1adcc2ee8d9afeb07733
Darshan <darshanbpatel1904@gmail.com>
2023-05-14 13:38:12 +0530

    deployment

--- models/connect.js
+++ models/connect.js
@@ -2,6 +2,13 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const connection = mysql.createPool(process.env.DATABASE_URL);
+export const connection = mysql.createPool(process.env.USER + process.env.PASSWORD + process.env.HOST + process.env.DATABASE + process.env.MYSQL_ATTR_SSL_CA);
 
-connection.getConnection();
+connection.getConnection().then((err,result)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(result);
+    }
+});
